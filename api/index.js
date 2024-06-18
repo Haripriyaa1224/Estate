901e131ca26bfceb388c2f31252197a846a40588
Haripriyaa <mailtohari1224@gmail.com>
2024-06-18 18:40:47 +0530

    Server Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv/config';
+// dotenv.config();
 
 //Connect to DB
 mongoose
