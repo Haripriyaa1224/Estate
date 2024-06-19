326cb9c6aadcfe632b73bb29efacda8916bdef22
Haripriyaa <mailtohari1224@gmail.com>
2024-06-19 23:40:57 +0530

    Update listing using UI completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,9 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
+import UpdateListing from "./pages/UpdateListing"
 import CreateListing from "./pages/createListing"
+
 import Header from "./components/Header"
 import PrivateRoute from "./components/PrivateRoute"
 
@@ -21,6 +23,10 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     
