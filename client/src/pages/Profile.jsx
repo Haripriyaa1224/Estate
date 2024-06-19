05a0574628813c35894eb09f2017ea737473cc8a
Haripriyaa <mailtohari1224@gmail.com>
2024-06-19 16:19:44 +0530

    Delete user API and functionality created

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -91,6 +94,24 @@ export default function Profile() {
     } catch (error) {
       dispatch(updateUserFailure(error.message));
     }
+    
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
   };
 
   return (
@@ -160,7 +181,7 @@ export default function Profile() {
       </p>
       <div className="flex justify-between mt-5">
         
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -34,6 +34,18 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    deleteUserStart: (state) => {
+        state.loading = true;
+      },
+      deleteUserSuccess: (state) => {
+        state.currentUser = null;
+        state.loading = false;
+        state.error = null;
+      },
+      deleteUserFailure: (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      },
   },
 });
 
@@ -42,6 +54,9 @@ export const { signInStart,
     signInFailure,
     updateUserFailure,
     updateUserSuccess,
-    updateUserStart, } = userSlice.actions;
+    updateUserStart,
+    deleteUserFailure,
+    deleteUserSuccess,
+    deleteUserStart, } = userSlice.actions;
 
 export default userSlice.reducer;
