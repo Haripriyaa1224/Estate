334220da80fc60c69997c7b18583c1161d73e450
Haripriyaa <mailtohari1224@gmail.com>
2024-06-20 22:03:51 +0530

    About Page added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Haven Horizon</h1>
+      <p className='mb-4 text-slate-700'>Haven Horizon is your trusted partner in navigating the exciting and sometimes complex world of buying, selling, or renting property. Our team of seasoned professionals boasts a deep understanding of the local market and a relentless dedication to exceeding your expectations.</p>
+      <p className='mb-4 text-slate-700'>
+      We believe in the power of personalized service and expert guidance. We take the time to understand your unique needs and goals, whether you're a first-time buyer, seasoned investor, or looking for the perfect rental. Our comprehensive approach ensures a smooth and successful experience every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>we're passionate about helping our clients achieve their real estate dreams. We believe buying or selling a property should be an empowering and rewarding journey. That's why we go the extra mile to provide insightful advice, clear communication, and unwavering support throughout the process.</p>
+    </div>
   )
 }
